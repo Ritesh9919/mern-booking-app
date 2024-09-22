5f27d30bc26ef4a24e12e5ee75e5b6c6cc530126
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-09-22 19:28:23 +0530

    adding layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Layout from "./layouts/Layout";
 function App() {
-  return <div className="text-center text-blue-500">App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout>Home Page</Layout>} />
+        <Route path="/search" element={<Layout>Search Page</Layout>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
