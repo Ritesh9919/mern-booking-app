198f0df69567fea0cd0c3187bcfe98a056797b00
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-09-22 18:21:41 +0530

    project setup

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGO_URI as string);
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/api/test", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.listen(7000, () => {
+  console.log(`Server is running on port:7000`);
+});
