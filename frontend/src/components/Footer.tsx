25797e5496fa6bde706281bbe23df78bc42e23c0
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-10-09 10:13:25 +0530

    improved ui

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
-    <div className="bg-blue-800 py-10">
-      <div className="container mx-auto flex justify-between items-center">
+    <div className="bg-blue-800 py-10 px-4">
+      <div className="container mx-auto flex flex-col lg:flex-row justify-center md:justify-between items-center">
         <span className="text-3xl text-white font-bold tracking-tight">
           MernHolidays.com
         </span>
--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,9 +1,11 @@
 function Hero() {
   return (
-    <div className="bg-blue-800 pb-16">
+    <div className="bg-blue-800 pb-16 px-4">
       <div className="container mx-auto flex flex-col gap-2">
-        <h1 className="text-5xl text-white font-bold">Find your next stay</h1>
-        <p className="text-2xl text-white">
+        <h1 className="text-5xl text-white font-bold text-center md:text-left">
+          Find your next stay
+        </h1>
+        <p className="text-2xl text-white text-center md:text-left">
           Search low prices on hotels for your dream vacation...
         </p>
       </div>
